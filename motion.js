85e2476dce1e0a6333e0e90078910e438490240a
art_ymst <captblue.edits@gmail.com>
2025-11-15 17:48:53 +0100

    Update motion.js

--- motion.js
+++ motion.js
@@ -1,8 +1,11 @@
-// Motion equations
-function getPosition(u, a, t) {
-  return u * t + 0.5 * a * t * t;
+function getPosition(u, a, t, tSegmentStart, uSegmentStart) {
+  // s = u*Δt + 0.5*a*Δt^2 starting from tSegmentStart
+  let dt = t - tSegmentStart;
+  return uSegmentStart + u * dt + 0.5 * a * dt * dt;
 }
 
-function getVelocity(u, a, t) {
-  return u + a * t;
+function getVelocity(u, a, t, tSegmentStart) {
+  // v = u + a*Δt
+  let dt = t - tSegmentStart;
+  return u + a * dt;
 }
