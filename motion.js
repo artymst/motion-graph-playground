17ff5d86068875b7bf0c9f694ad25f4b38918db4
art_ymst <captblue.edits@gmail.com>
2025-11-15 17:11:19 +0100

    Update motion.js

--- motion.js
+++ motion.js
@@ -1,8 +1,8 @@
-// Motion calculations
+// Motion equations
 function getPosition(u, a, t) {
   return u * t + 0.5 * a * t * t;
 }
+
 function getVelocity(u, a, t) {
   return u + a * t;
 }
-// You can export these if using ES Modules; otherwise, attach to window/global.
